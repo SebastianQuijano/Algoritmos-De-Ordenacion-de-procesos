2d98a5a1aadee7fff7a613c396ef22a27dfe0353
Drakensis <sebastian.quijanob@gmail.com>
2014-08-14 12:59:07 -0500

    Create ProcesoFcfs.java

---
+++ ProcesoFcfs.java
@@ -0,0 +1,55 @@
+package FCFS;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Brownie
+ */
+public class ProcesoFcfs {
+ 
+    private String nombre;
+    private int rafaga;
+    
+    
+    
+    public ProcesoFcfs(String nombre, int rafaga) {
+        this.rafaga = rafaga;
+        this. nombre = nombre;
+        
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the rafaga
+     */
+    public int getRafaga() {
+        return rafaga;
+    }
+
+    /**
+     * @param rafaga the rafaga to set
+     */
+    public void setRafaga(int rafaga) {
+        this.rafaga = rafaga;
+    }
+    
+}
+
